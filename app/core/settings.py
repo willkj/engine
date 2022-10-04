310c8c07e81794b157205ae675e38211596ee03f
willkj <wdwill55@gmail.com>
2022-10-04 13:40:14 -0300

    atualização

--- app/core/settings.py
+++ app/core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -205,6 +206,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
